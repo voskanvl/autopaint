7783691b24c4b159ae02edd6650a5a0d4b188775
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-06-15 16:57:42 +0300

    browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,34 @@ const sass = require("gulp-sass")(require("sass"));
 const del = require("del");
 const pug = require("gulp-pug");
 const include = require("gulp-include");
-const fileinclude = require("gulp-file-include");
 const webpack = require("webpack-stream");
 const named = require("vinyl-named");
 const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
 
 const DIST = "./dist";
 
-function clean(cb) {
-    del(["./dist"]);
+const clean = path => cb => {
+    del([path]);
     cb();
-}
+};
+
+const sync = () =>
+    browserSync.init({
+        server: {
+            baseDir: "dist",
+        },
+    });
 
 const jsTask = cb =>
     src("./src/js/pages/main.js")
-        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(
             webpack({
                 mode: "development",
             }),
         )
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write("."))
         .pipe(dest("./dist"));
 
 const pugTask = cb =>
@@ -31,16 +38,34 @@ const pugTask = cb =>
 
 const sassTask = cb =>
     src("./src/style/page/style.sass")
-        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(sass())
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write("."))
         .pipe(dest("./dist"));
 
-exports.watch = () => {
-    watch("./src/js/pages/index.js", jsTask);
-    watch("./src/pug/page/*", pugTask);
-    watch("./src/**/*.sass", sassTask);
+const watchTask = () => {
+    watch(
+        "./src/js/pages/index.js",
+        series(clean("./dist/index.js"), jsTask),
+    ).on("change", browserSync.reload);
+    watch("./src/pug/page/*", series(clean("./dist/*.html"), pugTask)).on(
+        "change",
+        browserSync.reload,
+    );
+    watch("./src/**/*.sass", series(clean("./dist/*.css"), sassTask)).on(
+        "change",
+        browserSync.reload,
+    );
 };
+
 exports.js = jsTask;
 
-exports.default = parallel(jsTask, pugTask, sassTask);
+exports.watch = watchTask;
+
+exports.browserSync = parallel(sync, watchTask);
+
+exports.default = parallel(
+    series(clean("./dist/index.js"), jsTask),
+    series(clean("./dist/*.html"), pugTask),
+    series(clean("./dist/*.css"), sassTask),
+);
