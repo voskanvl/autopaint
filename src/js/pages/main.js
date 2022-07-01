d6cbc39c8e93def1d7294495c5c95604ac4d1bb2
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-07-01 11:14:30 +0300

    layout splideCarousel

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -77,6 +77,16 @@ function start() {
                 "#15100A",
             ][newIndex]),
     );
+
+    const sliderCarousel = new Splide("#splideServices", {
+        type: "loop",
+        perPage: 1,
+        autoWidth: true,
+        arrowPath:
+            "M13 19.25C12.5858 19.25 12.25 19.5858 12.25 20C12.25 20.4142 12.5858 20.75 13 20.75L13 19.25ZM26.5303 20.5303C26.8232 20.2374 26.8232 19.7626 26.5303 19.4697L21.7574 14.6967C21.4645 14.4038 20.9896 14.4038 20.6967 14.6967C20.4038 14.9896 20.4038 15.4645 20.6967 15.7574L24.9393 20L20.6967 24.2426C20.4038 24.5355 20.4038 25.0104 20.6967 25.3033C20.9896 25.5962 21.4645 25.5962 21.7574 25.3033L26.5303 20.5303ZM13 20.75L26 20.75L26 19.25L13 19.25L13 20.75Z",
+        pagination: false,
+    }).mount();
+
     const closeModal = () => (modal.style.display = "none");
     const openModal = () => {
         modal.style.display = "block";
